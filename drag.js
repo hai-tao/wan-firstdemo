4b0192ffe40ed10d034de5b2d7d00a41634f5646
hai-tao <2689729249@qq.com>
2017-02-16 14:50:10 +0800

    add onmousemove

--- drag.js
+++ drag.js
@@ -1,12 +1,13 @@
 function drag(id){
  	var obj=document.getElementById(id);
- 	ogj.onmousedown=function(){
+ 	ogj.onmousedown=function(ev){
  		var disx=0;
  		var disy=0;
  		var wan="hai";
- 		obj.onmousedown=function(ev){
- 			disx=ev.pageX-obj.offsetLeft;
- 			disy=ev.pageY-obj.offsetTop;
- 		}
+ 		disx=ev.pageX-obj.offsetLeft;
+ 		disy=ev.pageY-obj.offsetTop;
+ 		document.onmousemove=function(){
+ 			var haha="haha"
+ 		};
  	}
 }
